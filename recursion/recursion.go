00d1a0c082a53abb45b2811009010295bd0b41e1
Sanam Limbu <sudosanam@gmail.com>
2022-11-12 20:03:52 +0800

    add recursive count

--- recursion/recursion.go
+++ recursion/recursion.go
@@ -7,3 +7,11 @@ func SumInt(arr []int) int {
 	}
 	return arr[0] + SumInt(arr[1:])
 }
+
+// CountInt - uses recursion to find number of items in array
+func CountInt(arr []int) int {
+	if len(arr) == 0 {
+		return 0
+	}
+	return 1 + CountInt(arr[1:])
+}
