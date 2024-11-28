dfc9821deeda2855267b2c405d6aabd8e0750157
Sanam Limbu <sudosanam@gmail.com>
2024-11-28 23:17:14 +0800

    commented time complexity

--- sort/insertion_sort.go
+++ sort/insertion_sort.go
@@ -5,6 +5,8 @@ import "github.com/sanamlimbu/dsa"
 // Insertion sort is a simple sorting algorithm that works by iteratively
 // inserting each element of an unsorted list into its correct position
 // in a sorted portion of the list. It is like sorting playing cards in your hands.
+//
+// Time complexity: O(n^2)
 func InsertionSort[T dsa.Ordered](input []T, sortType dsa.SortType) []T {
 	result := make([]T, len(input))
 	copy(result, input)
