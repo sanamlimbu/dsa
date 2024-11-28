4a85b95c81c1f8f174062292ea82fb045d7b51fa
Sanam Limbu <sudosanam@gmail.com>
2024-11-28 12:49:19 +0800

    added type constriant on merge sort and updated test cases

--- sort/merge_sort.go
+++ sort/merge_sort.go
@@ -1,43 +1,103 @@
 package sort
 
-/*
-* Merge sort algorithm is based on divide and conquer paradigm
-* time complexity is O(n log n)
- */
-func MergeSort(arr []int) []int {
-	if len(arr) < 2 {
-		return arr
+import "github.com/sanamlimbu/dsa"
+
+type SortType string
+
+const (
+	AscendingSortType  SortType = "ascending"
+	DescendingSortType SortType = "descending"
+)
+
+// Merge sort is a sorting algorithm that follows the divide-and-conquer approach.
+// It works by recursively dividing the input array into smaller subarrays
+// and sorting those subarrays then merging them back together to obtain the sorted array.
+// Time complexity: O(nlogn)
+func MergeSort[T dsa.Ordered](input []T, sortType SortType) []T {
+	if len(input) < 2 {
+		return input
 	}
+
+	// copying so input is not affected
+	arr := make([]T, len(input))
+	copy(arr, input)
+
 	midIndex := len(arr) / 2
 	leftHalf := arr[:midIndex]
 	rightHalf := arr[midIndex:]
 
-	leftHalf = MergeSort(leftHalf)
-	rightHalf = MergeSort(rightHalf)
+	// left half comes out sorted
+	leftHalf = MergeSort(leftHalf, sortType)
 
-	return merge(leftHalf, rightHalf)
+	// right half comes out sorted
+	rightHalf = MergeSort(rightHalf, sortType)
+
+	// combined value in descending order
+	if sortType == DescendingSortType {
+		return mergeDescending(leftHalf, rightHalf)
+	}
+
+	// combined value in ascending order
+	return mergeAscending(leftHalf, rightHalf)
 }
 
-func merge(leftHalf, rightHalf []int) []int {
-	result := []int{}
+// leftHalf and rightHalf are already in ascending order
+func mergeAscending[T dsa.Ordered](leftHalf, rightHalf []T) []T {
+	result := make([]T, 0)
 	leftSize := len(leftHalf)
 	rightSize := len(rightHalf)
 
-	i, j := 0, 0
+	var i, j int
+
 	for i < leftSize && j < rightSize {
-		if leftHalf[i] <= rightHalf[j] {
+		switch {
+		case leftHalf[i] <= rightHalf[j]:
 			result = append(result, leftHalf[i])
 			i++
-		} else {
+		default:
 			result = append(result, rightHalf[j])
 			j++
 		}
 	}
 
+	// merge remaining values from left half
 	for ; i < leftSize; i++ {
 		result = append(result, leftHalf[i])
 	}
 
+	// merge remaining values from right half
+	for ; j < rightSize; j++ {
+		result = append(result, rightHalf[j])
+	}
+
+	return result
+}
+
+// leftHalf and rightHalf are already in descending order
+func mergeDescending[T dsa.Ordered](leftHalf, rightHalf []T) []T {
+	result := make([]T, 0)
+	leftSize := len(leftHalf)
+	rightSize := len(rightHalf)
+
+	var i, j int
+
+	for i < leftSize && j < rightSize {
+		switch {
+		case leftHalf[i] < rightHalf[j]:
+			result = append(result, rightHalf[j])
+			j++
+		default:
+			result = append(result, leftHalf[i])
+			i++
+		}
+	}
+
+	// merge remaining values from left half
+	for ; i < leftSize; i++ {
+		result = append(result, leftHalf[i])
+	}
+
+	// merge remaining values from left half
 	for ; j < rightSize; j++ {
 		result = append(result, rightHalf[j])
 	}
--- sort/merge_sort_test.go
+++ sort/merge_sort_test.go
@@ -1,22 +1,92 @@
 package sort
 
 import (
+	"reflect"
 	"testing"
 )
 
-func TestMergeSort(t *testing.T) {
-	arr := []int{9, 1, 7, 5, 3, 6}
-	arrWanted := []int{1, 3, 5, 6, 7, 9}
-	arrGot := MergeSort(arr)
-	compare(arrWanted, arrGot)
+func TestMergeSortAscending(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []int
+		expected []int
+	}{
+		{
+			name:     "Sorted slice",
+			input:    []int{1, 2, 3, 4, 5},
+			expected: []int{1, 2, 3, 4, 5},
+		},
+		{
+			name:     "Unsorted slice",
+			input:    []int{4, 1, 5, 3, 2},
+			expected: []int{1, 2, 3, 4, 5},
+		},
+		{
+			name:     "Array with duplicates",
+			input:    []int{3, 1, 2, 3, 1},
+			expected: []int{1, 1, 2, 3, 3},
+		},
+		{
+			name:     "Empty slice",
+			input:    []int{},
+			expected: []int{},
+		},
+		{
+			name:     "Single element slice",
+			input:    []int{42},
+			expected: []int{42},
+		},
+	}
 
-	arr = []int{9, 8, 6, 3, 1}
-	arrWanted = []int{1, 3, 6, 8, 9}
-	arrGot = MergeSort(arr)
-	compare(arrWanted, arrGot)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := MergeSort(tt.input, AscendingSortType)
+			if !reflect.DeepEqual(result, tt.expected) {
+				t.Errorf("given: %v; got: %v; wanted: %v", tt.input, result, tt.expected)
+			}
+		})
+	}
+}
 
-	arr = []int{9, 1, 6, 7, 2}
-	arrWanted = []int{1, 2, 6, 7, 9}
-	arrGot = MergeSort(arr)
-	compare(arrWanted, arrGot)
+func TestMergeSortDescending(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []int
+		expected []int
+	}{
+		{
+			name:     "Sorted slice",
+			input:    []int{5, 4, 3, 2, 1},
+			expected: []int{5, 4, 3, 2, 1},
+		},
+		{
+			name:     "Unsorted slice",
+			input:    []int{4, 1, 5, 3, 2},
+			expected: []int{5, 4, 3, 2, 1},
+		},
+		{
+			name:     "Array with duplicates",
+			input:    []int{3, 1, 2, 3, 1},
+			expected: []int{3, 3, 2, 1, 1},
+		},
+		{
+			name:     "Empty slice",
+			input:    []int{},
+			expected: []int{},
+		},
+		{
+			name:     "Single element slice",
+			input:    []int{42},
+			expected: []int{42},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := MergeSort(tt.input, DescendingSortType)
+			if !reflect.DeepEqual(result, tt.expected) {
+				t.Errorf("given: %v; got: %v; wanted: %v", tt.input, result, tt.expected)
+			}
+		})
+	}
 }
